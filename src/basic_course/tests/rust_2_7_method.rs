f486d622877336d3f5c2ccf06e3dbbdb07a411a7
prj <prj06@qq.com>
2024-05-26 13:01:28 +0800

    method course

--- src/basic_course/tests/rust_2_7_method.rs
+++ src/basic_course/tests/rust_2_7_method.rs
@@ -1,5 +1,13 @@
 
 #[cfg(test)]
 pub mod tests {
-
+        use basic_course::rust_2_7_method::method;
+    #[test]
+    fn test_method(){
+        method::define_method();
+        method::multipart_param_method();
+        method::association_function();
+        method::multipart_impl();
+        method::impl_enum();
+    }
 }
